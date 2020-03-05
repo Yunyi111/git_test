23b2e38ed7f8e38b590f677d36c8bb19f430a934
y_yunyi <yaoyunyi1994@gmail.com>
2020-03-04 19:05:56 -0500

    initial commit

---
+++ week 8/11.py
@@ -0,0 +1,5 @@
+def describe_pet( animal_type='dog', pet_name):
+    print(pet_name)
+    print(animal_type)
+
+describe_pet(pet_name=111, animal_type="dolphin")
---
+++ week 8/main_program.py
@@ -0,0 +1,10 @@
+#way 1
+import math_functions as mf #give a name as mf
+
+print(mf.double_number(5))
+
+
+#way 2
+import math_functions
+
+print(math_functions.double_number(5))
